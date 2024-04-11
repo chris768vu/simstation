fd61dd34fb256eea680c31ee44920ee4130e20b8
chris768vu <40015054+chris768vu@users.noreply.github.com>
2024-04-10 23:04:07 -0700

    Implemented SimulationFactory

--- GroupAssignment4/src/simstation/SimulationFactory.java
+++ GroupAssignment4/src/simstation/SimulationFactory.java
@@ -1,1 +1,46 @@
+package simstation;
+import mvc.*;
 
+public abstract class SimulationFactory implements AppFactory{
+
+	@Override
+	public abstract Model makeModel();
+
+	@Override
+	public View makeView(Model m) {
+		return new SimulationView(m);
+	}
+
+	@Override
+	public abstract String getTitle();
+
+	@Override
+	public String[] getHelp() {
+		String[] help = {"Start starts the simulation\n", "Suspend pauses the simulation\n", "Resume undos suspend\n", 
+				"Stop ends the simulation run\n", 
+				"Stats displays the simulation time (in seconds) and the number of agents\n"};
+		return help;
+	}
+
+	@Override
+	public String about() {
+		return "Simstation";
+	}
+
+	@Override
+	public String[] getEditCommands() {
+		String[] commands = {"Start", "Suspend", "Resume", "Stop", "Stats"};
+		return commands;
+	}
+
+	@Override
+	public Command makeEditCommand(Model m, String name, Object source) {
+		if (name.equals("Start")) return new StartCommand(m);
+		if (name.equals("Stats")) return new StatsCommand(m);
+    	if (name.equals("Stop")) return new StopCommand(m);
+    	if (name.equals("Suspend")) return new SuspendCommand(m);
+    	if (name.equals("Resume")) return new ResumeCommand(m);
+		return null;
+	}
+
+}
