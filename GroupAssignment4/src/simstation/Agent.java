9a061080941aa8f5e8cb23f2acb13001adf36eae
Claireran520 <clairexiaokeran@gmail.com>
2024-04-15 00:41:53 -0700

    uploaded Agent.java

--- GroupAssignment4/src/simstation/Agent.java
+++ GroupAssignment4/src/simstation/Agent.java
@@ -1,73 +1,108 @@
 package simstation;
 
+import mvc.Utilities;
+
 import java.io.Serializable;
 
 public abstract class Agent implements Serializable, Runnable {
-	protected Simulation world;
-	
+	private static final int SLEEP = 50;
 	protected String name;
 	protected Heading heading;
-	protected int xc;
-	protected int yc;
-	protected boolean suspended = false;
-	protected boolean stopped = false;
+	protected double xc;
+	protected double yc;
+	protected Simulation world;
+	private boolean suspended = false;
+	private boolean stopped = false;
 	transient protected Thread myThread;
-	
+
+	public Agent(String name) {
+		this.name = name;
+	}
+
+	public Agent() {
+		//super();
+		suspended = false;
+		stopped = false;
+		myThread = null;
+
+		heading = Heading.random();
+		xc = Utilities.rng.nextInt(Simulation.SIZE);
+		yc = Utilities.rng.nextInt(Simulation.SIZE);
+	}
+
+	@Override
 	public void run() {
 		myThread = Thread.currentThread();
+		onStart();
 		while (!stopped) {
-		    try {
-		    	onStart();
-		    	onInterrupted();
-		    	
-		        update();
-		        Thread.sleep(20);
-		        checkSuspended();
-		        
-		        onExit();
-		    } catch(InterruptedException e) {
-		       e.getMessage();
-		    }
+			try {
+				update();
+				Thread.sleep(SLEEP);
+				checkSuspended();
+			} catch (InterruptedException e) {
+				Utilities.error(e);
+			}
 		}
-    }
-	
-	 private synchronized void checkSuspended() {
-		try {
-		    while(!stopped && suspended) {
-		        wait();
-		        suspended = false;
-		    }
-		} catch (InterruptedException e) {
-		   e.getMessage();
-		}
-	 }
-	 
-	 // is called by run() and will be overridden by subclasses?
-	public abstract void update();
-	
-	/* "At strategic places Agent.run() calls empty methods: onStart, onInterrupted, and OnExit. 
-	 * These can be overridden in subclasses if needed."
-	 */
-	public void onStart() {}
-	public void onInterrupted() {}
-	public void onExit() {}
+		onExit();
+		world.changed();
+	}
 
-	public void start() {}
-	
-	public void suspend() {}
-	
-	public void resume() {}
-	
-	public void stop() {}
-	
-	public void move(int steps) {
+	public synchronized void start() {
+		myThread = new Thread(this);
+		myThread.start();
+	}
+
+	public synchronized void suspend() {
+		suspended = true;
+	}
+
+	public synchronized void resume() {
+		suspended = false;
+		notify();
+	}
+
+	public synchronized void stop() {
+		stopped = true;
+	}
+
+	public abstract void update();
+
+
+	public void setWorld(Simulation world) {
+		this.world = world;
+	}
+
+	public void onStart() {
+	}
+	public void onInterrupt(){
+
+	}
+	public void onExit() {
+	}
+
+	private synchronized void checkSuspended() {
+		try {
+			while (!stopped && suspended) {
+				onInterrupt();
+				wait();
+				suspended = false;
+			}
+		} catch (InterruptedException e) {
+			Utilities.error(e);
+		}
+	}
+	public synchronized void move(int steps) {
 		for (int i = 0; i < steps; i++) {
-			/* add or substract to xc or yc depending on this.heading...
-			 * 
-			 * so we need our latest Heading.java in to the repository,
-			 * because I think someone already did it
-			 */
+			xc = (xc + Simulation.SIZE + heading.getxDrt()) % Simulation.SIZE;
+			yc = (yc + Simulation.SIZE + heading.getyDrt()) % Simulation.SIZE;
 			world.changed();
 		}
+	}
+	public synchronized double getXc() {
+		return xc;
+	}
+
+	public synchronized double getYc() {
+		return yc;
 	}
 }
