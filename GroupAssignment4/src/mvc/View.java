0c17a92ffc8529b68b77b4b9aaf82af94389360c
Claireran520 <clairexiaokeran@gmail.com>
2024-04-16 14:58:16 -0700

    Update AppPanel.java and View.java

--- GroupAssignment4/src/mvc/View.java
+++ GroupAssignment4/src/mvc/View.java
@@ -1,30 +1,35 @@
-package mvc; 
+package mvc;
+
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class View extends JPanel implements Subscriber {
-
     protected Model model;
 
-    
     public View(Model model) {
-    	this.model = model;
-    	model.subscribe(this);
+        this.model = model;
+        model.subscribe(this);
+        setBorder(new LineBorder(Color.black));
     }
 
-    // Subscriber update method
-    
-    public void update() {}
-    
-    public void setModel(Model newModel) {
-        if (this.model != null) {
-            this.model.unsubscribe(this);
+    public void setModel(Model model) {
+        // remove previous connection
+        this.model.unsubscribe(this);
+
+        for (Component c : this.getComponents()) {
+            if (c instanceof View) {
+                ((View) c).setModel(model);
+            }
         }
-        
-        this.model = newModel;
-        if (this.model != null) {
-            this.model.subscribe(this);
-            update(); 
-        }
+
+        this.model = model;
+        model.subscribe(this);
+        repaint();
+    }
+    
+    @Override
+    public void update() {
+        repaint();
     }
 }
