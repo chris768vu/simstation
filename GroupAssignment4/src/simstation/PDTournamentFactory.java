d56a99c4de9449f76e337d1842062762e0bcb7ac
chris768vu <40015054+chris768vu@users.noreply.github.com>
2024-04-17 12:26:05 -0700

    Delete GroupAssignment4/src/simstation/PDTournamentFactory.java

--- GroupAssignment4/src/simstation/PDTournamentFactory.java
+++
@@ -1,9 +0,0 @@
-package pdtournament;
-
-import mvc.*;
-import simstation.*;
-
-public class PDTournamentFactory extends SimulationFactory {
-	public Model makeModel() {return new PDTournamentSimulation();}
-	public String getTitle() {return "Prisoner's Dilemma Tournament";}
-}
